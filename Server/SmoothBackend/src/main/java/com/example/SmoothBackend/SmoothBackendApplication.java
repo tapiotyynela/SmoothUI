064519a80a42958f354306d062a3a3442ee86e0e
tapio.tyynela <tapio.tyynela@barona.fi>
2021-02-14 16:10:56 +0200

    initialize backend

---
+++ Server/SmoothBackend/src/main/java/com/example/SmoothBackend/SmoothBackendApplication.java
@@ -0,0 +1,16 @@
+package com.example.SmoothBackend;
+
+import java.io.Console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmoothBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmoothBackendApplication.class, args);
+		System.out.println("MOIKKA");
+	}
+
+}
---
+++ Server/SmoothBackend/src/test/java/com/example/SmoothBackend/SmoothBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmoothBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmoothBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
