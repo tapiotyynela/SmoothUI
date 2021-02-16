fac175476904291089cd8eae4d5b08e5b1e1b2ac
tapio.tyynela <tapio.tyynela@barona.fi>
2021-02-16 18:52:33 +0200

    connected smooths together

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 function Header() {
+	const [backendText, setBackendText] = useState('')
+
+	useEffect(() => {
+		axios.get('/instructors/testdata/tests')
+		.then(response => setBackendText(JSON.stringify(response.data)))
+	}, [])
+
 	return (
 		<>
 			<h1>Olen header</h1>
+			<p>{backendText}</p>
 		</>
 	);
 }
