1af81c1bc8753a71664806d194b666e050d26906
tapiotyynela <tapio.tyynela@gmail.com>
2021-03-08 19:34:26 +0200

    converted react frontend to typescript

---
+++ dist/components/Trophies.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(require("react"));
+var react_2 = require("@chakra-ui/react");
+var Trophies = function () {
+    return (<>
+			<react_2.Box bg="blue" w="100%" p={4} color="white"></react_2.Box>
+		</>);
+};
+exports.default = Trophies;
--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import axios from "axios";
+import axios, { ResponseType } from "axios";
 import { Box, FormLabel, Input, Button } from "@chakra-ui/react";
 
 const Games = () => {
